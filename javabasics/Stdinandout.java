03c8185763624eea094c804820f57039927de02a
atvc0202 <atulvc2001@gmail.com>
2022-04-12 20:59:03 +0530

    added Stdinandout.java

---
+++ javabasics/Stdinandout.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int myInt1 = sc.nextInt();
+        int myInt2 = sc.nextInt();
+        int myInt3 = sc.nextInt();
+        
+        System.out.println(myInt1);
+        System.out.println(myInt2);
+        System.out.println(myInt3);
+        
+    }
+}
