253d03080c5f9fb1034c4188ec6bf9ae4f300670
atvc0202 <atulvc2001@gmail.com>
2022-04-13 20:48:15 +0530

    added a couple more programs from hackerrank

---
+++ javabasics/stdinandout2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        float b = sc.nextFloat();
+        sc.nextLine();     
+        String str = sc.nextLine(); 
+        
+        
+        System.out.println("String: " + str);
+        System.out.println("Double: " + b);
+        System.out.println("Int: " + a);
+        
+    }
+}
