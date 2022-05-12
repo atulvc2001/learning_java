d047cc9e4c41031beadbdc919f91e7588f523d02
atvc0202 <atulvc2001@gmail.com>
2022-05-12 23:50:57 +0530

    Created a new folder for college programs and added the arraySort program as the first addition

---
+++ TYLpracticeprobs/Arrays/ArraySort.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class ArraySort {
+
+	public static void main(String[] args){
+		
+		int[] myarray1 = {1789,2035,1899,1456,2013,1458,2458,1254,1472,2365,1456,2156,1457,2456};
+
+		String[] myarray2 = {
+			"java","python","php","C#","C programming","C++"
+		};
+		
+		System.out.println("Original numeric array: " + Arrays.toString(myarray1));
+		Arrays.sort(myarray1);
+		System.out.println("Sorted numeric array: " + Arrays.toString(myarray1));
+
+		System.out.println("Original String array: " + Arrays.toString(myarray2));
+		Arrays.sort(myarray2);
+		System.out.println("Sorted String array: " + Arrays.toString(myarray2));
+	}
+}
